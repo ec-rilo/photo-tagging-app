16317649dcbe04144b5c1967063a4fc2f9066278
Edgar <ecarrillo046@gmail.com>
2022-01-03 14:44:59 -0800

    feat: add StyledSelectionContainer and styled Home Container element

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import StyledHeader from '../components/Header';
+import StyledSelectionContainer from '../components/SelectionContainer';
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ margin: '0 20px' }}>
       <StyledHeader />
+      <StyledSelectionContainer />
     </div>
   );
 };
