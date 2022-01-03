a9fe4685366d94939a73b87362152e153821293a
Edgar <ecarrillo046@gmail.com>
2022-01-03 12:40:10 -0800

    feat: style logo for small devices

---
+++ src/components/Logo.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import device from '../assets/Data/DeviceSizes';
+
+const Logo = ({ className }) => {
+  return (
+    <div className={className}>
+      <p>Where's (?)</p>
+    </div>
+  );
+};
+
+const StyledLogo = styled(Logo)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--gunmetal);
+  border: 1px solid var(--clr-milk-white);
+  color: var(--clr-milk-white);
+  font-family: var(--fnt-regular);
+
+  @media ${device.mobileS} {
+    width: 260px;
+    height: 60px;
+    font-size: 2.5rem;
+  }
+`;
+
+export default StyledLogo;
