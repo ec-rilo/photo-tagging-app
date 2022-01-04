65fe7d668eb509514258407f47d4b3d8b805fb5b
Edgar <ecarrillo046@gmail.com>
2022-01-04 15:01:49 -0800

    feat: add Welcome page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import StyledHeader from '../components/Header';
 import StyledSelectionContainer from '../components/SelectionContainer';
+import StyledWelcomeContainer from '../components/WelcomeContainer';
 
 const Home = () => {
   return (
     <div style={{ margin: '0 20px' }}>
       <StyledHeader />
       <StyledSelectionContainer />
+      <StyledWelcomeContainer />
     </div>
   );
 };
