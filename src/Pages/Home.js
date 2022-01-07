976536f89e656478fe5dc1a6a97e7a4fdc58d9b0
Edgar <ecarrillo046@gmail.com>
2022-01-06 18:51:57 -0800

    feat: group welcomecontainer and leaderboard. also add 2 new states

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,41 @@
+import { useCallback, useState } from 'react';
 import StyledHeader from '../components/Header';
+import StyledLeaderboard from '../components/Leaderboard';
 import StyledSelectionContainer from '../components/SelectionContainer';
 import StyledWelcomeContainer from '../components/WelcomeContainer';
 
 const Home = () => {
+  const [playBtn, setPlayBtn] = useState({ isActive: true });
+  const [closeBtn, setCloseBtn] = useState({ isActive: false });
+
+  const toggleSelection = useCallback(
+    (btn1, btn2) => {
+      if (btn1.isActive) {
+        setPlayBtn({ isActive: true });
+        setCloseBtn({ isActive: false });
+      } else if (btn2.isActive) {
+        setPlayBtn({ isActive: false });
+        setCloseBtn({ isActive: true });
+      }
+    },
+    [setPlayBtn, setCloseBtn]
+  );
+
   return (
     <div style={{ margin: '0 20px' }}>
       <StyledHeader />
-      <StyledSelectionContainer />
-      <StyledWelcomeContainer />
+      <StyledSelectionContainer toggleSelection={toggleSelection} />
+      <div
+        style={{
+          marginTop: '50px',
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <StyledWelcomeContainer playBtn={playBtn} />
+        <StyledLeaderboard homepage closeBtn={closeBtn} />
+      </div>
     </div>
   );
 };
