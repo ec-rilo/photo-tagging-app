72baf76317f4999e83eef7aa7fe1a2d9050ccc7d
Edgar <ecarrillo046@gmail.com>
2022-01-09 10:46:22 -0800

    feat: import HomePage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,23 +1,6 @@
 import { useCallback, useState } from 'react';
-import styled from 'styled-components';
-import device from '../assets/Data/DeviceSizes';
-import StyledHeader from '../components/Header';
-import StyledLeaderboard from '../components/Leaderboard';
-import StyledSelectionContainer from '../components/SelectionContainer';
-import StyledWelcomeContainer from '../components/WelcomeContainer';
-import Game from './Game';
 import { HomeProvider } from './HomeContext';
-
-const StyledContainer = styled.div`
-  margin: 100px auto 0 auto;
-  position: relative;
-  display: flex;
-  max-width: 600px;
-
-  @media ${device.mobileL} {
-    margin: 50px auto 0 auto;
-  }
-`;
+import HomePage from './HomePage';
 
 const Home = () => {
   const [playBtn, setPlayBtn] = useState({ isActive: true });
@@ -45,12 +28,11 @@ const Home = () => {
           maxWidth: '2000px',
         }}
       >
-        <StyledHeader />
-        <StyledSelectionContainer toggleSelection={toggleSelection} />
-        <StyledContainer>
-          <StyledLeaderboard homepage closeBtn={closeBtn} />
-          <StyledWelcomeContainer homepage playBtn={playBtn} />
-        </StyledContainer>
+        <HomePage
+          toggleSelection={toggleSelection}
+          playBtn={playBtn}
+          closeBtn={closeBtn}
+        />
       </div>
     </HomeProvider>
   );
