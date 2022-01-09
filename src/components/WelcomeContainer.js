8abff845a4f74461adc879504659638057c27549
Edgar <ecarrillo046@gmail.com>
2022-01-08 20:04:38 -0800

    feat: made welcome container responsive

--- src/components/WelcomeContainer.js
+++ src/components/WelcomeContainer.js
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import device from '../assets/Data/DeviceSizes';
 import StyledForm from './WelcomeForm';
 
 const StyledTitle = styled.h2`
-  font-size: 2rem;
+  font-size: 3rem;
+
+  @media ${device.mobileL} {
+    font-size: 2.5rem;
+  }
+
+  @media ${device.mobileS} {
+    font-size: 2rem;
+  }
 `;
 
 const IntroSection = styled.section`
@@ -11,12 +20,22 @@ const IntroSection = styled.section`
   gap: 10px;
 `;
 
+const StyledSubTitle = styled.p`
+  font-size: 1.2rem;
+
+  @media ${device.mobileL} {
+    font-size: 1rem;
+  }
+`;
+
 const WelcomeContainer = ({ className }) => {
   return (
     <div className={className}>
       <IntroSection>
         <StyledTitle>Welcome</StyledTitle>
-        <p>To start playing, please enter your name here.</p>
+        <StyledSubTitle>
+          To start playing, please enter your name here.
+        </StyledSubTitle>
       </IntroSection>
       <StyledForm />
     </div>
@@ -31,11 +50,17 @@ const StyledWelcomeContainer = styled(WelcomeContainer)`
   pointer-events: auto;
   transition: opactiy 0.3s;
 
+  ${(props) =>
+    props.homepage &&
+    css`
+      flex-grow: 1;
+    `}
+
   ${({ playBtn }) => {
     if (!playBtn.isActive) {
       return `
       opacity: 0;
-      pointer-events: none;  
+      pointer-events: none;
     `;
     }
   }}
